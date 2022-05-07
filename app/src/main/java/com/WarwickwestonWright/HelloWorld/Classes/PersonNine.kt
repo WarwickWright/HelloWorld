fe303027c88055e49489f0dd3ba6ef986fc378cc
Warwick Weston Wright <67828052+WarwickWright@users.noreply.github.com>
2022-05-07 13:00:40 +0100

    Delete app/src/main/java/com/WarwickwestonWright/HelloWorld directory
    To be repushed

--- app/src/main/java/com/WarwickwestonWright/HelloWorld/Classes/PersonNine.kt
+++
@@ -1,9 +0,0 @@
-package com.WarwickWestonWright.HelloWorld.Classes
-
-class PersonNine(name: String, age: Int) : PersonEight(name, age) {
-
-    fun personGetNine() : Int {
-        return 9
-    }
-
-}
